ed0ce93d920bb9768c21e9c939624f1969063446
MikhailFrenkel <frenkel.misha@mail.ru>
2018-07-13 13:22:42 +0300

    Added model

---
+++ TestingSystem/TestingSystem.Model/Question.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingSystem.Model
+{
+    class Question
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string CorrectAnswer { get; set; }
+        public int TestId { get; set; }
+    }
+}
---
+++ TestingSystem/TestingSystem.Model/Theme.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingSystem.Model
+{
+    class Theme
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
