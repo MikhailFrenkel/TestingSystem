11e5f9a3272459c539765f71bb7e4be3cd64db13
MikhailFrenkel <frenkel.misha@mail.ru>
2018-07-13 13:48:31 +0300

    Added interfaces IRepository, IUnitOfWork

---
+++ TestingSystem/TestingSystem.DataProvider/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingSystem.Model;
+
+namespace TestingSystem.DataProvider.Interfaces
+{
+    interface IUnitOfWork : IDisposable
+    {
+        IRepository<Theme> Themes { get; set; }
+        IRepository<Test> Tests { get; set; }
+        IRepository<Question> Questions { get; set; }
+        void Save();
+    }
+}
